b9af67ba355310863ffaebcc1bf2e4da4af8af18
Cameron Higby-Naquin <me@chigby.org>
2022-03-27 20:04:02 -0400

    Remember color theme across pages
    Minification by https://www.digitalocean.com/community/tools/minify

--- theme.js
+++ theme.js
@@ -1,25 +1,7 @@
-function setTheme(value) {
-  if (value === 'system') {
-    delete document.documentElement.dataset.colorTheme;
-  } else {
-    document.documentElement.dataset.colorTheme = value;
-  }
-  localStorage.setItem('theme', value);
-}
-
 document.querySelectorAll('input[name="theme-toggle"]').forEach(element => {
   element.addEventListener("change", e => {
     e.preventDefault();
     let value = document.querySelector('input[name="theme-toggle"]:checked').value;
-    setTheme(value);
-  })
+    window.__setPreferredTheme(value);
+  });
 });
-
-let theme = localStorage.getItem('theme');
-if (theme) {
-  let box = document.getElementById(`theme-toggle-${theme}`);
-  if (box) {
-    box.checked = true;
-  }
-  setTheme(theme);
-}
