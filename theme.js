d1f92e6e2cb09b27be40ec570a02e4f3a3492cbf
Cameron Higby-Naquin <me@chigby.org>
2022-03-27 20:07:31 -0400

    Preserve checked state of preferred theme input

--- theme.js
+++ theme.js
@@ -1,4 +1,6 @@
+let preferredTheme = JSON.parse(localStorage.getItem('theme'));
 document.querySelectorAll('input[name="theme-toggle"]').forEach(element => {
+  if (element.value == preferredTheme) { element.checked = true; }
   element.addEventListener("change", e => {
     e.preventDefault();
     let value = document.querySelector('input[name="theme-toggle"]:checked').value;
