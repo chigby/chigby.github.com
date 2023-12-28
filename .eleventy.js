e8f18ccadd96b17fb4f3f900c2f35acc16bd8e95
Cameron Higby-Naquin <me@chigby.org>
2023-12-28 17:47:35 -0500

    Unify JS color theme choosing code

--- .eleventy.js
+++ .eleventy.js
@@ -41,7 +41,6 @@ module.exports = function(eleventyConfig) {  // Set custom directories for input
   eleventyConfig.addPassthroughCopy("src/fonts");
   eleventyConfig.addPassthroughCopy("src/img");
   eleventyConfig.addPassthroughCopy("src/vid");
-  eleventyConfig.addPassthroughCopy("src/js");
   eleventyConfig.addPassthroughCopy("src/**/*.txt");
   eleventyConfig.addPassthroughCopy("src/favicon.ico");
   eleventyConfig.addPassthroughCopy("src/CNAME");
