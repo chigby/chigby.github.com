f04bb80ccf62005e72a11843025c3429ec14b49d
Cameron Higby-Naquin <me@chigby.org>
2023-12-28 17:11:53 -0500

    Add light/dark theme switcher to Rime layout

---
+++ src/js/theme-chooser.js
@@ -0,0 +1,26 @@
+function setTheme(value) {
+  if (["light", "dark"].includes(value)) {
+    document.documentElement.dataset.colorTheme = value;
+    localStorage.setItem('theme', value);
+  } else if (value === "system") {
+    delete document.documentElement.dataset.colorTheme;
+  }
+}
+
+export default function() {
+  let theme = localStorage.getItem('theme');
+  if (["light", "dark"].includes(theme)) {
+    document.documentElement.dataset.colorTheme = theme;
+    let box = document.getElementById(theme);
+    if (box) {
+      box.checked = true;
+    }
+  }
+
+  const themeButtons = document.querySelectorAll('input[name="theme-toggle"]');
+  themeButtons.forEach(button => {
+    button.addEventListener("click", () => {
+      setTheme(button.value);
+    });
+  });
+};
