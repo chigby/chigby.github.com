afda57cd43abbc86d7dedba0de87d99c2a26b94f
Cameron Higby-Naquin <me@chigby.org>
2024-01-01 12:53:05 -0500

    Commit changes to docs

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -1,1 +1,30 @@
-(()=>{function o(e){["light","dark"].includes(e)?(document.documentElement.dataset.colorTheme=e,localStorage.setItem("theme",e)):e==="system"&&delete document.documentElement.dataset.colorTheme}function m(){let e=localStorage.getItem("theme");if(["light","dark"].includes(e)){document.documentElement.dataset.colorTheme=e;let t=document.getElementById(e);t&&(t.checked=!0)}document.querySelectorAll('input[name="theme-toggle"]').forEach(t=>{t.addEventListener("click",()=>{o(t.value)})})}m();})();
+(() => {
+  // src/js/theme-chooser.js
+  function setTheme(value) {
+    if (["light", "dark"].includes(value)) {
+      document.documentElement.dataset.colorTheme = value;
+      localStorage.setItem("theme", value);
+    } else if (value === "system") {
+      delete document.documentElement.dataset.colorTheme;
+    }
+  }
+  function theme_chooser_default() {
+    let theme = localStorage.getItem("theme");
+    if (["light", "dark"].includes(theme)) {
+      document.documentElement.dataset.colorTheme = theme;
+      let box = document.getElementById(theme);
+      if (box) {
+        box.checked = true;
+      }
+    }
+    const themeButtons = document.querySelectorAll('input[name="theme-toggle"]');
+    themeButtons.forEach((button) => {
+      button.addEventListener("click", () => {
+        setTheme(button.value);
+      });
+    });
+  }
+
+  // src/js/index.js
+  theme_chooser_default();
+})();
